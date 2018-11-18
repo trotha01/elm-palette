c6d7996ea61c8ee81b7dbfbbb80535c03bf88ede
trotha01 <trevor.rothaus@sendgrid.com>
2018-11-18 11:12:59 -0800

    adding travis ci for gh-pages

---
+++ bin/gh-pages.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+BIN_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+PROJECT_DIR="$(dirname "$BIN_DIR")"
+EXAMPLE_DIR="$PROJECT_DIR/examples/"
+
+# compile
+$PROJECT_DIR/bin/build.sh
+
+# remove *.js from .gitignore
+# compatible with both GNU and BSD/Mac sed
+sed -i.bak '/\*.js/d' .gitignore
+rm .gitignore.bak
+
+echo "ls"
+ls
+
+# init branch and commit
+git init
+git config user.name "Trevor Rothaus (via Travis CI)"
+git config user.email "trotha01@gmail.com"
+git checkout -b gh-pages
+git add .
+git status
+git commit -m "Deploy to GitHub Pages [skip ci]"
+git push --force "https://${GITHUB_TOKEN}@github.com/trotha01/elm-aframe.git" gh-pages
